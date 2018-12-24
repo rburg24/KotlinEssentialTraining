c53357d4fc8d19695fbeb76f2d17ea1436c4b5b2
Ramon Burgos <rburg24@gmail.com>
2018-12-24 00:13:20 -0500

    fixed

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,23 @@
+
+val myName = "Ramon"
+
 fun main(args: Array<String>) {
+
+
+    var num1 = 45.0
+
+    println("The initial value of num1 is $num1")
+
+    num1++
+
+    println("The new value of num1 is $num1")
+
+
+    var num2:Double = 5.toDouble()
+
+
+    println("The  value of num2 is $num2")
+
+    println("My name is $myName")
 
 }
