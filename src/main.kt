be3a3ce7ae6686da7855822ad19f11530385be03
Ramon Burgos <rburg24@gmail.com>
2018-12-25 17:17:55 -0500

    fixed

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,17 @@
 fun main(args: Array<String>) {
     val num1 = 15
-    val num2 = 10
+    val num2 = 15
+
+
+    val match = (num1 == num2)
+
+    println("Match = $match")
+
+    val match2 = num1.equals(num2)
+
+    println("Match2 = $match2")
+
+
+
+
 }
