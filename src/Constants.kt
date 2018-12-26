9884fa4cceb21d80dcad91e8f3e3b0c9f4458829
Ramon Burgos <rburg24@gmail.com>
2018-12-26 15:32:59 -0500

    fixed

---
+++ src/Constants.kt
@@ -0,0 +1,12 @@
+class Constants{
+
+    companion object {
+        @JvmField
+        val RED = "Red"
+
+
+        const val BLUE = "blue"
+
+    }
+
+}
--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,9 @@
+import Constants.Companion.BLUE
+import Constants.Companion.RED
+
 fun main(args: Array<String>) {
+
+
+    println("The color is $RED and $BLUE")
 
 }
