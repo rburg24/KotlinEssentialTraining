b41c413f9f5682a9bfe168f18291b24a0abd110c
Ramon Burgos <rburg24@gmail.com>
2018-12-26 15:07:34 -0500

    fixed

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,34 @@
 fun main(args: Array<String>) {
 
+
+    var aString = "Hello!"
+
+    println(aString)
+
+
+    var empty = String()
+
+    println("'$empty'")
+
+
+    val charArray = aString.toCharArray()
+
+    println(charArray.toList())
+    val byteArray = aString.toByteArray()
+
+    println(byteArray.toList())
+
+
+    aString += " and Welcome!"
+
+    println(aString)
+
+    val len = aString.length
+
+    for (i in 0 until len){
+        val c = aString.get(i)
+        println(c)
+    }
+
+
 }
