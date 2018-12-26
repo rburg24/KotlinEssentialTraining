c88357f9770ed46d930fdb18e37b56f6d3760182
Ramon Burgos <rburg24@gmail.com>
2018-12-25 22:19:29 -0500

    fixed

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,33 @@
 fun main(args: Array<String>) {
-    val num1 = 15
+
+    var num1 = 15
+
     val num2 = 10
+
+    val sum = num1 + num2
+
+    println("sum = $sum")
+
+    val sum2 = num1.plus(num2)
+
+    println("sum2 = $sum2")
+
+    val diff = num1 - num2
+
+    println("diff = $diff")
+
+    val diff2 = num1.minus(num2)
+
+    println("diff2 = $diff2")
+
+    num1 ++
+
+    println("num1 = $num1")
+
+    num1.inc()
+
+    println("num1 = $num1")
+
+
+
 }
