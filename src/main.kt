89afeaeb5a7c3b9b96d263bc1b2e3a688192ba81
Ramon Burgos <rburg24@gmail.com>
2018-12-26 15:17:34 -0500

    fixed

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,17 @@
+
+
 fun main(args: Array<String>) {
+
+
+    val builder = StringBuilder("To be or not to be \n")
+
+            .append("2-- \n")
+            .append("3-- \n")
+            .append("4-- \n")
+            .append("5-- \n")
+
+    var result = builder.toString()
+
+    println(result)
 
 }
