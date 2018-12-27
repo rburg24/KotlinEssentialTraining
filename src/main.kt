ba6b35693a338774efb643e1cd5c58486766b0cf
Ramon Burgos <rburg24@gmail.com>
2018-12-26 21:55:16 -0500

    fixed

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,33 @@
 fun main(args: Array<String>) {
 
+    var nullValue:String? = null
+
+
+    println(nullValue)
+
+
+    val l:Int? = nullValue?.length
+
+    println("Length is $l")
+
+
+    var message = if(l!=null) {
+        "Length: $l"
+    } else{
+        "l is null"
+    }
+    println(message)
+
+    var l2 = l?: -1
+
+    println("The value of l2 is $l2")
+
+    try {
+        val l3 = l !!
+        println("The value of l3 is $l3")
+    } catch (e: KotlinNullPointerException) {
+        println("We have an error:: ${e.localizedMessage}")
+
+    }
+
 }
