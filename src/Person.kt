5f568ce4e3de9c69e59885b38ac86896d52891aa
Ramon Burgos <rburg24@gmail.com>
2018-12-28 18:21:22 -0500

    fixed

---
+++ src/Person.kt
@@ -0,0 +1,8 @@
+data class Person (private val firstName:String,
+                   private val lastName: String){
+
+
+    val fullName: String
+        get() = "$firstName $lastName"
+
+}
--- src/main.kt
+++ src/main.kt
@@ -6,5 +6,20 @@ fun main(args: Array<String>) {
     println(item)
 
     val item2 = ClothingItem("M", 14.99)
+
+
+    item2.price = 10.0
+
+
     println(item2)
+
+    println("Item type = ${item2.type}")
+    println("Item price = ${item2.price}")
+
+    val person = Person("John","Smith")
+
+    println(person)
+    println(person.fullName)
+
+
 }
